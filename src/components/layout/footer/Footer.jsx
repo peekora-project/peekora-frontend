174d35ed9cf98b7be82caf63a2e61d4acae62775
mac <Hasancanmidi@hotmail.com>
2024-03-24 13:22:39 +0300

    Footer tamamlandı

---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import './Footer.css'
+
+export default function Footer() {
+  return (
+  <div className='fixed-footer'>
+   <MDBContainer className='p-4 pb-0'> 
+    <MDBFooter className='bg-dark text-center text-white'>
+      
+        <section className='mb-4 pt-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+        </section>
+      
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2024 Copyright:
+        <a className='text-white' href='https://peekora.com/'>
+          peekora.com
+        </a>
+      </div>
+    </MDBFooter>
+    </MDBContainer>
+    </div>
+  );
+}
